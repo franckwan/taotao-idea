eecba944fc7366f4a4f2050d6fe8061c7ec6518b
wansu <franck@wansudeMacBook-Pro.local>
2017-01-09 11:45:11 +0800

    add
    add

---
+++ taotao/taotao-manager-web/src/main/java/com/taotao/controller/PageController.java
@@ -0,0 +1,18 @@
+package com.taotao.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by franck on 17/1/8.
+ */
+@Controller
+public class PageController {
+    /**
+     * 返回首页
+     */
+    @RequestMapping("/")
+    public String showIndex(){
+        return "index";
+    }
+}
